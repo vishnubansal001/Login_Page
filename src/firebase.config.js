00ea594255e360d17df53ca1a05466fcf1bcffbb
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-05-21 02:21:30 +0530

    added the env file and removed the api key

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGZK7JplyYXT8-cUsikeupUOBFuQUjwvo",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "otp-login-4c3c2.firebaseapp.com",
   projectId: "otp-login-4c3c2",
   storageBucket: "otp-login-4c3c2.appspot.com",
