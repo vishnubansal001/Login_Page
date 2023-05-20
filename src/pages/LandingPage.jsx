f8f3b6b3e729955f3460447c24e76653a5fc0951
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-05-21 02:14:27 +0530

    landing page after the sign in page have been completed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
     <>
-      
+      <section className="bg-[#757272] flex items-center justify-center h-screen">
+        <h1 className="text-white font-bold lg:text-5xl md:text-3xl text-2xl">
+          Login Complete
+        </h1>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
